3e3a73a921c32a2fd10ed626b627fc08efbd1df0
Jan Egil Ødegård <j.e.odegard@fys.uio.no>
2023-03-21 17:21:07 +0100

    Moving more stuff about

--- substorm_pred.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import h5py
-
-path_feats = '/scratch/oath_v1.1/features/auroral_feat.h5'
-path_classification = '/scratch/oath_v1.1/classifications/classifications.csv'
-path_substorms = '/scratch/substorms/substorm_list.csv'
-
