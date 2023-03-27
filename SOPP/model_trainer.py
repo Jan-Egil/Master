43be1ce24dfe179b39ac52b6d594e398666fb09d
Jan Egil Ødegård <j.e.odegard@fys.uio.no>
2023-03-27 20:54:47 +0200

    Prepared stuff

---
+++ SOPP/model_trainer.py
@@ -0,0 +1,11 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+
+image_path = ''
+model_path = ''
+
+# Import images from hdf-file
+
+# Train model using PCA and SVM
+
+# Export model to file
