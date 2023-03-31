f6eb351d8497e402ebcf7032318d36d28527e260
Jan Egil Ødegård <j.e.odegard@fys.uio.no>
2023-03-31 18:59:10 +0200

    Started working on preprocessing script

--- SOPP/preproc_imgs_train.py
+++ SOPP/preproc_imgs_train.py
@@ -5,6 +5,12 @@ Script for pre-processing all-sky images to correct format
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from PIL import Image
+import os
+
+os.chdir('../data')
+path = os.getcwd()
+print(path)
 
 path_img_info = '/scratch/oath_v1.1/classifications/classifications.csv'
 path_unprocessed_imgs = '/scratch/oath_v1.1/cropped_scaled'
@@ -12,10 +18,15 @@ path_processed_imgs = '/scratch/SOPP/processed_imgs'
 
 # Fetch information about processing said image
 
-df = pd.read_csv(path_img_info, header=18)
+df = pd.read_csv(path_img_info, header=16)
 print(df)
+rot_angle = np.array(df['rotAng'])
 
-# Fetch image from path
+# Fetch image from path. Apply rotation. Place image in new path
+png = '.png'
+for img_number in range(1, rot_angle.shape[0]+1):
+    filename = str(img_number).zfill(5) + '.png'
+    print(filename)
 
 # Rotation
 
