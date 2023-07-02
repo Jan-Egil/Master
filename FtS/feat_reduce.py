a255d8b0599b55289fc10f6ee51b80058ce7d667
Jan Egil Ødegård <janegil.ode@gmail.com>
2023-07-02 15:54:26 +0200

    Finished reducing features, next step: binning in 1 minute iterations

--- FtS/feat_reduce.py
+++ FtS/feat_reduce.py
@@ -8,63 +8,64 @@ from datetime import datetime
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
-array_feats_first = np.random.random((100,1000))
-loc = ['fsim' for i in range(100)]
-timestamps = [datetime.now() for i in range(100)]
-
-dict = {"features": [array_feats_first[0]],
-        "timestamp": timestamps[0],
-        "loc": loc[0]}
-df = pd.DataFrame(dict, dtype=object)
-
-
-for i in tqdm(range(1,100)):
-    dict = {"features": [array_feats_first[i]],
-            "timestamp": timestamps[i],
-            "loc": loc[i]}
-    df2 = pd.DataFrame(dict)
-    df = pd.concat([df, df2], ignore_index=True)
-    df.reset_index()
-
-del df2
-
-# 0th: Define the paths
-"""
 if platform == 'win32':
-    curr_path = os.getcwd()
-    temp_dir = "\\tempdir"
-    full_path = curr_path+temp_dir
-    cdfpath = full_path + "\\temp.cdf"
-    save_path = ...
+    array_feats_first = np.random.random((100,1000))
+    loc = ['fsim' for i in range(100)]
+    timestamps = [datetime.now() for i in range(100)]
+
+    dict = {"features": [array_feats_first[0]],
+            "timestamp": timestamps[0],
+            "loc": loc[0]}
+    df = pd.DataFrame(dict, dtype=object)
+    print("Placing randomly generated features in dataframe..")
+    for i in tqdm(range(1,100)):
+        dict = {"features": [array_feats_first[i]],
+                "timestamp": timestamps[i],
+                "loc": loc[i]}
+        df2 = pd.DataFrame(dict)
+        df = pd.concat([df, df2], ignore_index=True)
+        df.reset_index()
+
+    del df2
+    del array_feats_first
+    del loc
+    del timestamps
+    print("Placement done!\n")
 else:
+    # 0th: Define the paths
     curr_path = os.getcwd()
     temp_dir = "/tempdir"
     full_path = curr_path+temp_dir
     cdfpath = full_path + "/temp.cdf"
     save_path = "/scratch/feats_FtS/"
+    save_path_reduced = save_path + "reduced_feats.h5"
 
-# 1st: Gather all features in one large array-like structure
-# Tip: first make one big pandas dataframe, then turn that into array
-print("Extracting the data from file")
-df = pd.read_hdf(save_path+f"iter_0_loc_fsim.h5", key='features')
+    # 1st: Gather all features in one large array-like structure
+    # Tip: first make one big pandas dataframe, then turn that into array
+    print("Extracting the data from file")
+    df = pd.read_hdf(save_path+f"iter_0_loc_fsim.h5", key='features')
 
-for iter in tqdm(range(1,1686)):
-    df2 = pd.read_hdf(save_path+f"iter_{iter}_loc_fsim.h5", key='features')
-    df = pd.concat([df, df2], ignore_index=True)
-    df.reset_index()
-del df2
-print("Data extraction done!\n")
-"""
+    for iter in tqdm(range(1,1686)):
+        df2 = pd.read_hdf(save_path+f"iter_{iter}_loc_fsim.h5", key='features')
+        df = pd.concat([df, df2], ignore_index=True)
+        df.reset_index()
+    del df2
+    print("Data extraction done!\n")
+    
 
 # Define and extract features-column into its own array
 num_points = len(df.index)
 num_feats = 1000
 array_feats = np.zeros((num_points, num_feats))
+timestamps = []
+loc = []
 
-for i in range(num_points):
-    features = df['features'][i]
-    array_feats[i] = features
-del features
+print("Extracting data from dataframe into separate arrays..")
+for i in tqdm(range(num_points)):
+    array_feats[i] = df['features'][i]
+    timestamps.append(df['timestamp'][i])
+    loc.append(df['loc'][i])
+print("Extraction from dataframe done!\n")
 
 # 2nd: Apply scaling and PCA to reduce said dimensions. (35 dims is a nice start)
 # HEADS UP! One should maybe train-test-split first before scaling and reducing to be statistically correct.
@@ -80,15 +81,36 @@ reducer = PCA(n_components=35)
 array_feats = reducer.fit_transform(array_feats)
 print("Reduction done!\n")
 
-print(array_feats[0].shape)
-
 # 3rd: Connect the right features with the right locatons and timestamps in a new dataframe
 
+print("Placing reduced features, timestamps and location in new dataframe..")
+dict = {"feat_reduced": [array_feats[0]],
+        "timestamp": timestamps[0],
+        "loc": loc[0]}
+new_df = pd.DataFrame(dict, dtype=object)
 
+for i in tqdm(range(1, num_points)):
+    dict = {"feat_reduced": [array_feats[i]],
+            "timestamp": timestamps[i],
+            "loc": loc[i]}
+    new_df2 = pd.DataFrame(dict)
+    new_df = pd.concat([new_df, new_df2], ignore_index=True)
+    new_df.reset_index()
+del new_df2
+print("Placement done!")
+
+print(new_df)
 
 # 4th: Save dataframe to file. Delete variable
 # Tip: Try to plot features against timestamps. See if you find some structure in the madness
 
+print("Placing dataframe in hdf-file")
+if platform == "win32":
+    new_df.to_hdf("reduced_feats.h5", key=f'reduced_feats', mode='w')
+else:
+    new_df.to_hdf(save_path_reduced, key=f'reduced_feats', mode='w')
+
+del new_df
 
 # 5th: Extract dataframe from file
 
