a8165684c3e8d1c059de93d06275e7789e7aaea5
Jan Egil Ødegård <j.e.odegard@fys.uio.no>
2023-07-12 19:18:15 +0200

    Ready to work from home the rest of the evening

--- FtS/model_creator.py
+++ FtS/model_creator.py
@@ -1,4 +1,14 @@
+import os
 import numpy as np
 import pandas as pd
+from sys import platform
 
 master_df_path = "/scratch/feats_FtS/master_df/master_fsim.h5"
+master_df = pd.read_hdf(master_df_path, key=f"final_feats")
+master_df.sort_values(by='timestamp', inplace=True, ignore_index=True)
+
+# Step 1: Extract into different arrays
+
+# Step 2: split training and testing data in smart manner
+# Tip 1: 30/70 split
+# Tip 2: k-fold cross-validation
--- FtS/temp_data_parser.py
+++ FtS/temp_data_parser.py
@@ -1,16 +1,41 @@
 import numpy as np
 import h5py
 import pandas as pd
+import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-path = "/scratch/feats_FtS/"
+master_df_path = "/scratch/feats_FtS/master_df/master_fsim.h5"
+master_df = pd.read_hdf(master_df_path, key=f"final_feats")
+master_df.sort_values(by='timestamp', inplace=True, ignore_index=True)
 
-df = pd.read_hdf(path+f"iter_0_loc_fsim.h5", key='features')
+# Extract data into individual arrays
 
-for iter in tqdm(range(1,1686)):
-    df2 = pd.read_hdf(path+f"iter_{iter}_loc_fsim.h5", key='features')
-    #print(df2)
-    df = pd.concat([df, df2], ignore_index=True)
-    df.reset_index()
-print(df)
-print(df.columns)
+num_feats = master_df['averaged_feats'][0].shape[0]
+num_imgs = len(master_df.index)
+
+array_feats = np.zeros((num_imgs, num_feats))
+substorm_onset = np.zeros(num_imgs)
+timestamps = np.zeros(num_imgs)
+print(master_df.columns)
+
+for i in tqdm(range(num_imgs)):
+    array_feats[i] = master_df['averaged_feats'][i]
+    substorm_onset[i] = master_df['substorm_onset'][i]
+    #timestamps[i] = master_df['timestamp'][i]
+
+
+array_feats_for_plotting = array_feats.T
+
+for i in tqdm(range(num_imgs)):
+    if substorm_onset[i] == 1:
+        for j in range(30,35):
+            y_data = array_feats_for_plotting[j][i-60:i+60]
+            plt.plot(y_data, label=f"feature {j}")
+        plt.axvline(60+15, color='b', linestyle="--")
+        plt.axvline(60, color='b', linestyle='--')
+        break
+#plt.legend()
+plt.grid()
+plt.show()
+
+# Plot each feature 2 hour before substorm, until 2 hour after substorm, for first substorm.
